91c87de1e9356c621b049b777450a6b7600b8fc9
Silantiev Jan <Silantiev-999@mail.ru>
2025-02-11 11:30:48 +0300

    relis3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("hideButton").addEventListener("click", function () {
-  var brandButtons = document.querySelectorAll(".brand-button");
+  var brandButtons = document.querySelectorAll(".brand-button:nth-last-child(-n+3)");
   brandButtons.forEach(function (button) {
     button.style.display = button.style.display === "none" ? "flex" : "none";
   });
