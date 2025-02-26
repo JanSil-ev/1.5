5c3159ef13824b515f3aed67f360cf7f1c60b480
Silantiev Jan <Silantiev-999@mail.ru>
2025-02-26 17:50:17 +0300

    relis5

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
+var x = window.matchMedia("(max-width: 321px)");
+myFunction(x);
+function myFunction(x) {
+  if (x.matches) {
+    let swiper = new Swiper(".mySwiper", {
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
+  } else {
+    document.querySelector(".swiper-wrapper").classList.add("brands");
+    document
+      .querySelector(".swiper-wrapper")
+      .classList.remove("swiper-wrapper");
+
+    document.querySelector(".swiper").classList.remove("swiper", "mySwiper");
+
+    document.querySelectorAll(".swiper-slide").forEach((el) => {
+      el.classList.add("brands-high");
+      el.classList.remove("swiper-slide");
+    });
+  }
+}
+
 document.getElementById("hideButton").addEventListener("click", function () {
-  const child = window.matchMedia('(min-width: 768px)').matches ? 3 : 5;
+  const child = window.matchMedia("(min-width: 769px)").matches ? 3 : 5;
   const brandButtons = document.querySelectorAll(
-    `.brand-button:nth-last-child(-n+${child})`  );
+    `.brands-high:nth-last-child(-n+${child})`
+  );
 
   brandButtons.forEach(function (button) {
     button.style.display = button.style.display === "flex" ? "none" : "flex";
   });
-  this.textContent = this.textContent === "Скрыть" ? "Показать" : "Скрыть";
-});
+  this.textContent = this.textContent === "Скрыть" ? "Показать все" : "Скрыть";
+  hideButton.classList.toggle("tap");
 
+  document
+    .querySelector(".hideButton::before")
+    .classList.add("hideButton--clicked::before");
+});
